bf347ac75f1192453422c7b7b1c575e9dd43ea00
ziadOUA <ziadouapro@outlook.com>
2023-01-20 23:55:28 +0100

    Various optimizations

--- ressourcerie/scripts/script.js
+++ ressourcerie/scripts/script.js
@@ -78,6 +78,6 @@ function contactSelected() {
 }
 
 function bodyLoaded() {
-	const body = document.getElementById('body');
+	const body = document.querySelector('body');
 	body.style.opacity = '1';
 }
